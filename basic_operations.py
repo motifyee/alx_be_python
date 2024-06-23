c21f1da50b16b0a48ed1298fa408ceeb0eab2e23
mrt <iquixe@gmail.com>
2024-06-23 06:23:53 +0300

    create basic_operations.py

---
+++ basic_operations.py
@@ -0,0 +1,10 @@
+number1 = 10
+number2 = 5
+
+sub = number1 - number2
+add = number1 + number2
+mul = number1 * number2
+
+print("Addition of " + str(number1) + " and " + str(number2) + " is " + str(add))
+print("Subtraction of " + str(number1) + " and " + str(number2) + " is " + str(sub))
+print("Multiplication of " + str(number1) + " and " + str(number2) + " is " + str(mul))
