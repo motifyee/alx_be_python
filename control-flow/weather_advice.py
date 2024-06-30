29c136280d796f0257fe440abbdf80bc09c0217d
mrt <iquixe@gmail.com>
2024-06-30 05:11:40 +0300

    weather_advice.py replace match with if

--- control-flow/weather_advice.py
+++ control-flow/weather_advice.py
@@ -1,12 +1,12 @@
 
 curr_weather = input("What's the weather like today? (sunny/rainy/cold):")
 
-match curr_weather:
-    case "sunny":
-        print("Wear a t-shirt and sunglasses.")
-    case "rainy":
-        print("Don't forget your umbrella and a raincoat.")
-    case "cold":
-        print("Make sure to wear a warm coat and a scarf.")
-    case _:
-        print("Sorry, I don't have recommendations for this weather.")
+
+if curr_weather == "sunny":
+    print("Wear a t-shirt and sunglasses.")
+elif curr_weather == "rainy":
+    print("Don't forget your umbrella and a raincoat.")
+elif curr_weather == "cold":
+    print("Make sure to wear a warm coat and a scarf.")
+else:
+    print("Sorry, I don't have recommendations for this weather.")
