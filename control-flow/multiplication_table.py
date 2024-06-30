3b278ed3397e3cd65115a912341e6e8a47309a14
mrt <iquixe@gmail.com>
2024-06-30 05:25:21 +0300

    fix range

--- control-flow/multiplication_table.py
+++ control-flow/multiplication_table.py
@@ -1,5 +1,5 @@
 
 number = int(input("Enter a number to see its multiplication table:"))
 
-for i in range(10):
+for i in range(1, 11):
     print(str(number) + " * " + str(i) + " = " + str(number * i))
