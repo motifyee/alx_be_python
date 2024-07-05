e2bdcc105334e2a69e4e9bfc882b9329fe76a144
mrt <iquixe@gmail.com>
2024-07-05 15:09:56 +0300

    fix

--- fns_and_dsa/arithmetic_operations.py
+++ fns_and_dsa/arithmetic_operations.py
@@ -11,4 +11,5 @@ def perform_operation(num1, num2, operation):
         case 'divide':
             if num2 == 0:
                 return "cannot divide by zero"
-            return num1 / num2
+            elif num2 != 0:
+                return num1 / num2
