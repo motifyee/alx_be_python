730ed869ab4672d2669f15fe62b33238621486fb
mrt <iquixe@gmail.com>
2024-07-20 02:39:22 +0300

    fix

---
+++ programming_paradigm/library_management.py
@@ -0,0 +1,22 @@
+class Book:
+    _is_checked_out = False
+
+    def __init__(self, title, author) -> None:
+        self.title = title
+        self.author = author
+
+
+class Library:
+    _books = []
+
+    def add_book():
+        _book.append(Book())
+
+    def check_out_book(title):
+        pass
+
+    def return_book(title):
+        pass
+
+    def list_available_books():
+        pass
