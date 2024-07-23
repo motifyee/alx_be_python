e62a95b436b499da73c36480b6fe449baf82a4c3
mrt <iquixe@gmail.com>
2024-07-24 00:33:42 +0300

    add test_simple_calculator.py

---
+++ programming_paradigm/test_simple_calculator.py
@@ -0,0 +1,27 @@
+import unittest
+from simple_calculator import SimpleCalculator
+
+
+class TestCalc(unittest.TestCase):
+    def test_add(self):
+        res = SimpleCalculator().add(222, 333)
+        self.assertEqual(res, 555)
+
+    def test_subtract(self):
+        res = SimpleCalculator().subtract(333, 222)
+        self.assertEqual(res, 100)
+
+    def test_multiply(self):
+        res = SimpleCalculator().multiply(10, 20)
+        self.assertEqual(res, 200)
+
+    def test_divide(self):
+        res = SimpleCalculator().divide(90, 10)
+        self.assertEqual(res, 9)
+
+        with self.assertRaises(ZeroDivisionError):
+            SimpleCalculator().divide(90, 0)
+
+
+if __name__ == "__main__":
+    unittest.main()
